ffada22863fbbc61c9a0e55e1c0f3a4743186f4a
Tzila-m <194138992+Tzila-m@users.noreply.github.com>
2026-04-20 15:11:10 +0300

    update server file

--- src/server.py
+++ src/server.py
@@ -1,6 +1,8 @@
 from mcp_app import mcp
-from tools import system_tools  # noqa: F401
-from tools import process_tools  # noqa: F401
+from tools import system_tools 
+from tools import process_tools  
+from tools import file_tools 
+from resources import file_resource
 
 if __name__ == "__main__":
     mcp.run()
